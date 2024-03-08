4b7c3eb10f9dfc656cc7801881e3cfce0112e95d
Tran Tri <vnsolerdie@gmail.comi>
2024-03-08 18:57:34 +0700

    oke

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -190,8 +190,10 @@ $invitation = config('data.invitation');
             const customHost = document.querySelector('input[name="host"]').value || '';
             const customHost2 = document.querySelector('input[name="host2"]').value || '';
             if (customGuest && customHost && customHost2) {
+                customHost = encodeURIComponent(customHost);
+                customGuest = encodeURIComponent(customGuest);
+                customHost2 = encodeURIComponent(customHost2);
                 customData = `&host1=${customHost}&guest=${customGuest}&host2=${customHost2}`;
-                customData = encodeURIComponent(customData);
             }
         }
         const copyLink = document.querySelector('#copyLink');
